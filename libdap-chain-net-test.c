9bf18f61da8764a819951649ca6018aca9248631
Aleksandr Lysikov <lysikov@inbox.ru>
2018-12-13 15:18:34 +0500

    ping realisation in progress

--- libdap-chain-net-test.c
+++ libdap-chain-net-test.c
@@ -1,12 +1,17 @@
-#include <stdbool.h>
 #include <stdio.h>
-#include "dap_chain_net.h"
-#include "dap_common.h"
 
-int main()
+#include "dap_config.h"
+#include "dap_chain_net.h"
+#include "iputils/ping.h"
+
+int main() //int argc, char **argv)
 {
+    const char *ip = "192.168.2.2";
+    int repeat = 3;
 //    dap_common_init("net-test.log");
-    dap_config_init("./dist");
-    dap_chain_net_init();
-    printf("test %d\n", 1);
+//    dap_config_init("./dist");
+//    dap_chain_net_init();
+    int res = ping_util(ip, repeat);
+    printf("ping_util()=%d\n", res);
+    return 0;
 }
