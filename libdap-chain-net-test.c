b0a7f9a76287457cc82e51693cc43f855663fa0f
Aleksandr Lysikov <lysikov@inbox.ru>
2018-12-14 14:57:16 +0500

    make ping_util4() & ping_util6()

--- libdap-chain-net-test.c
+++ libdap-chain-net-test.c
@@ -2,16 +2,20 @@
 
 #include "dap_config.h"
 #include "dap_chain_net.h"
-#include "iputils/ping.h"
+#include "iputils/iputils.h"
 
 int main() //int argc, char **argv)
 {
-    const char *ip = "192.168.2.2";
-    int repeat = 3;
+    const char *ip = "ya.ru";
+    //const char *ip = "::1";//"2001:4860:4860::8888";//"::1";
+
 //    dap_common_init("net-test.log");
 //    dap_config_init("./dist");
 //    dap_chain_net_init();
-    int res = ping_util(ip, repeat);
-    printf("ping_util()=%d\n", res);
+
+
+    int repeat = 3;
+    int res = ping_util4(ip, repeat);
+    printf("ping_util(%s, %d)=%.3lf msec\n", ip, repeat, res / 1000.);
     return 0;
 }
